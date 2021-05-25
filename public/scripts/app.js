2e86ec2ead63b3b47e15716ed03882518d4edcf3
Tomas Storc <tomas.storc@gmail.com>
2021-05-25 16:35:50 +0200

    portfolio hover effect on desktop, and mobile version without hover

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,11 @@ import typedText from "./typedtext.js";
 import mobileMenu from "./mobileMenu.js";
 import sendForm from "./sendForm.js";
 import setYear from "./getYear.js";
+import addText from "./addText.js";
 
 sendForm();
 typedText();
 animations();
 mobileMenu();
 setYear();
+addText();
